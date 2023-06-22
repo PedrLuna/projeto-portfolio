cc2aa1722d47b4725f658c702f23b8c097b4bd65
Pedro Lucas Luna <92765211+PedrLuna@users.noreply.github.com>
2023-06-22 20:42:55 -0300

    Update social-btns.tsx

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -8,13 +8,13 @@ import "./social-btns.scss"
 export function SocialBtns(){
     return(
         <div className="social">
-            <a href="https://www.instagram.com/pedr_luna/">
+            <a href="https://www.instagram.com/pedr_luna/" target="_blank">
                 <InstaIcon/>
             </a>
-            <a href="https://www.linkedin.com/in/pedro-lucas-luna-araujo-vieira-6678b6216/">
+            <a href="https://www.linkedin.com/in/pedro-lucas-luna-araujo-vieira-6678b6216/" target="_blank">
                 <LinkedInIcon/>
             </a>
-            <a href="https://github.com/pedr_luna">
+            <a href="https://github.com/pedr_luna" target="_blank">
                 <GitHubIcon/>
             </a>
 
